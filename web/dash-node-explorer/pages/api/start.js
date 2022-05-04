fe59d213a04b9291a7c4858ac62b2de0666a1368
dowl <xpatek08@stud.fit.vutbr.cz>
2022-05-04 11:29:24 +0200

    WEB - added starttime, basic peer fetching

---
+++ web/dash-node-explorer/pages/api/start.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+    const resp = await fetch("http://localhost:5000/");
+    const json = await resp.json();
+
+    res.status(200).json(json);
+}
--- web/dash-node-explorer/pages/index.js
+++ web/dash-node-explorer/pages/index.js
@@ -1,13 +1,42 @@
 import { AppShell, Box, Button, Header, Navbar, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
 
-export default function Home() {
+export default function Home({ start, _now }) {
+    const [startDate, setStartDate] = useState(new Date(start));
+    const [now, setNow] = useState(new Date(_now));
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            const resp = await fetch("/api/peers");
+            const peers_json = await resp.json();
+            console.log(peers_json?.count);
+            console.log(peers_json?.peers[1]);
+        };
+
+        const dateUpdate = async () => setNow(new Date());
+
+        const int = setInterval(() => {
+            fetchApi();
+        }, 1000 * 10);
+
+        const int2 = setInterval(() => {
+            dateUpdate();
+        }, 1000 * 1);
+
+        return () => {
+            clearInterval(int);
+            clearInterval(int2);
+        };
+    }, []);
+
     return (
         <AppShell
             padding="md"
             navbar={
-                <Navbar width={{ base: 200 }} p="xs" style={{ display: "flex" }}>
+                <Navbar width={{ base: 300 }} p="xs" style={{ display: "flex" }}>
                     {/* Navbar content */}
-                    <Text>Started: 12/05/2022</Text>
+                    <Text mt={"md"}>{`Started: ${startDate.toLocaleString()}`}</Text>
+                    <Text>{`Running for ${Math.floor((now.getTime() - startDate.getTime()) / 1000)} seconds.`}</Text>
                 </Navbar>
             }
             header={
@@ -36,4 +65,12 @@ export default function Home() {
             </Box>
         </AppShell>
     );
+}
+
+export async function getServerSideProps({ req, res }) {
+    const message = await fetch("http://localhost:3000/api/start");
+    const json = await message.json();
+    return {
+        props: { ...json, _now: new Date().toISOString() },
+    };
 }
